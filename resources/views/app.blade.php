56389bebd32ad061874baa9d9686e1834244750f
Mountaga Diao Leye Diop <mdl.diop97@gmail.com>
2026-05-26 22:34:35 +0000

    feat(brand): add favicon set under public/favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,9 +38,11 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/favicons/favicon.ico" sizes="any">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png">
+        <link rel="manifest" href="/favicons/site.webmanifest">
 
         @fonts
 
